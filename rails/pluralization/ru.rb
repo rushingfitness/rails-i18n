8f86254dae70f375815ded838ef2478e609a85d5
David Chen <dave.t.chen@gmail.com>
2016-10-31 21:47:30 -0400

    Change :ru pluralization to one_other

--- rails/pluralization/ru.rb
+++ rails/pluralization/ru.rb
@@ -1,3 +1,7 @@
-require 'rails_i18n/common_pluralizations/east_slavic'
+# require 'rails_i18n/common_pluralizations/east_slavic'
+#
+# ::RailsI18n::Pluralization::EastSlavic.with_locale(:ru)
 
-::RailsI18n::Pluralization::EastSlavic.with_locale(:ru)
+require 'rails_i18n/common_pluralizations/one_other'
+
+::RailsI18n::Pluralization::OneOther.with_locale(:ru)
